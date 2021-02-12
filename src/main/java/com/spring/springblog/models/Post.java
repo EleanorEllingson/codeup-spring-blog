47c4efc749c4290a56b7a0da2e6579ceb61bc49b
Eleanor Ellingson <eleanor.ellingson@gmail.com>
2021-02-12 10:28:06 -0600

    added springblog db and added tables

--- src/main/java/com/spring/springblog/models/Post.java
+++ src/main/java/com/spring/springblog/models/Post.java
@@ -1,9 +1,20 @@
 package com.spring.springblog.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "posts")
 public class Post {
-    private String title;
-    private String body;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(nullable = false)
+        private String title;
+
+    @Column(nullable = false)
+        private String body;
+
 
     public Post() {
     }
---
+++ src/main/java/com/spring/springblog/repositories/PostRepository.java
@@ -0,0 +1,8 @@
+package com.spring.springblog.repositories;
+
+import com.spring.springblog.models.Dog;
+import com.spring.springblog.models.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
