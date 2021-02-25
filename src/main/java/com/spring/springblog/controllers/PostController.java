8e318e753e5f323773c303f29192240144054368
Eleanor Ellingson <eleanor.ellingson@gmail.com>
2021-02-25 10:48:52 -0600

    added mail functionality

--- src/main/java/com/spring/springblog/controllers/PostController.java
+++ src/main/java/com/spring/springblog/controllers/PostController.java
@@ -4,6 +4,7 @@ import com.spring.springblog.models.Post;
 import com.spring.springblog.models.User;
 import com.spring.springblog.repositories.PostRepository;
 import com.spring.springblog.repositories.UserRepository;
+import com.spring.springblog.services.EmailService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -15,10 +16,12 @@ public class PostController {
 
     private final PostRepository postsDao;
     private final UserRepository userDao;
+    private final EmailService emailService;
 
-    public PostController(PostRepository postsDao, UserRepository userDao) {
+    public PostController(PostRepository postsDao, UserRepository userDao, EmailService emailService) {
         this.postsDao = postsDao;
         this.userDao = userDao;
+        this.emailService = emailService;
     }
 
     @GetMapping("/posts")
@@ -78,15 +81,20 @@ public class PostController {
 
     @PostMapping("/posts/create")
     public String createPost(@ModelAttribute Post post) {
-//        Post post = new Post();
-//        post.setTitle(title);
-//        post.setBody(body);
-//
-//
+
         User user = userDao.findAll().get(0);
         post.setUser(user);
 
-        postsDao.save(post);
+        Post savedPost = postsDao.save(post);
+
+//        Send an email once ad is saved
+
+        String subject = "New Post Created!";
+
+        String body = "Dear " + savedPost.getUser().getUsername() + ". Thank you for creating a post. Your post id is: " + savedPost.getId();
+
+        emailService.prepareAndSend(savedPost, subject, body);
+
         return "redirect:/posts";
     }
 
